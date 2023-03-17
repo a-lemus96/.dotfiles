85996df57fe1fba3ef5ae5149345d5d5b586773c
a-lemus96 <alejandro.lemus@cimat.mx>
2023-03-17 12:36:46 -0600

    feat: Clone plugins repo and git submodules
    Update setup.sh script to create target folder and clone plugin
    repositories to be used.

--- setup.sh
+++ setup.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 # File: setup.sh
 
-dir=~/.config/nvim/
+base_dir=~/.config/nvim/
 
-if [[ -d  $dir ]]
+if [[ ! -d  $base_dir ]] # test if base_dir does not exist
 then
-	cp init.vim $dir
-else
-	echo Directory $dir does not exist
-fi 
+	mkdir -p init.vim $base_dir
+fi
+
+cp init.vim $base_dir # copy source files into their target folders
+start_path=$(realpath $base_dir/pack)
+
+# clone plugin repository and initialize git submodules
+if [[ ! $(ls -A $start_path) ]]
+then
+	git clone git@github.com:a-lemus96/nvim-pack.git $start_path --recurse-submodules
+fi
